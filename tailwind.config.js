33883b35ba86d19a649086837af6e8cf03b43627
Jacob Rushlow <gheezl25@gmail.com>
2023-08-27 13:37:18 -0400

    Finally got tailwind working properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js, ts, jsx, tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -37,4 +45,3 @@ module.exports = {
   },
   plugins: [],
 }
-
